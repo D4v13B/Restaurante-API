32d43c08231c17f490fc693afc4db7a954650ab4
David Bustamante <66386035+D4v13B@users.noreply.github.com>
2024-11-14 23:22:09 -0500

    fix return IActionResultMethod to use HttpResponseCode

--- Restaurante/Controllers/UsuarioController.cs
+++ Restaurante/Controllers/UsuarioController.cs
@@ -15,37 +15,37 @@ namespace Restaurante.Controllers
         }
 
         [HttpPost]
-        public object SaveUser(UsuarioRequest usuario)
+        public IActionResult SaveUser(UsuarioRequest usuario)
         {
             string[] datos = { usuario.Nombre, usuario.Email, usuario.UsuarioTipoId.ToString() };
             //Vamos a verificar primero
             if (datos.Any(string.IsNullOrEmpty)) 
             {
-                return new
+                return BadRequest(new
                 {
                     titulo = "Error al guardar",
                     msg = "Llenar todos los campos",
                     Code = 400
-                };
+                });
             }
 
             int verifySave = new Db().SaveUser(usuario);
             if (verifySave > 0)
             {
-                return new
+                return Ok (new
                 {
                     titulo = "Guardado correctamente",
                     msg = "El usuario se ha guardado exitosamente",
                     Code = 200
-                };
+                });
             }
 
-            return new
+            return BadRequest(new
             {
                 titulo = "Error al guardar",
                 msg = "Los datos explotaron",
                 Code = 400
-            };
+            });
         }
     }
 }
